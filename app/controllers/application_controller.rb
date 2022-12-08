d7d19e301ec64c181e1fd47c3adfcff433dec5c9
Sonu Buddh <veeraryan92@gmail.com>
2022-12-08 13:07:33 +0530

    rescue error not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found
+    render 'globals/not_found'
+  end
 end
